a74e7b0018823c2af4cea1e4390ca4252d892ac0
Cesar Sanchez <cesasanc@c1r6p18.hive.fi>
2024-12-13 13:43:45 +0200

    More changes

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,9 +6,39 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 13:13:38 by cesasanc          #+#    #+#             */
-/*   Updated: 2024/12/13 13:21:00 by cesasanc         ###   ########.fr       */
+/*   Updated: 2024/12/13 13:41:15 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(const std::string &name)
+	: name(name), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+	std::cout << "ClapTrap " << name << " has been created" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap " << name << " has been destroyed" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+	: name(other.name), hitPoints(other.hitPoints), energyPoints(other.energyPoints), attackDamage(other.attackDamage)
+{
+	std::cout << "ClapTrap " << name << " has been copied" << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &other)
+{
+	if (this != &other)
+	{
+		name = other.name;
+		hitPoints = other.hitPoints;
+		energyPoints = other.energyPoints;
+		attackDamage = other.attackDamage;
+	}
+	std::cout << "Clap Trap " << name << " has been assigned" << std::endl;
+	return (*this);
+}
 
